c0c0707788413dc46086bd9b03e3285a73edb7cd
PalMachulla <73520862+PalMachulla@users.noreply.github.com>
2025-11-10 17:12:16 +0100

    fix: Update menu item from 'My Stories' to 'My Canvases'
    - Update canvas page menu item
    - Update characters page menu item
    - Consistent with the canvases terminology used throughout the app

--- frontend/src/app/canvas/page.tsx
+++ frontend/src/app/canvas/page.tsx
@@ -853,7 +853,7 @@ export default function CanvasPage() {
                   New Canvas
                 </button>
 
-                {/* My Stories */}
+                {/* My Canvases */}
                 <button
                   onClick={() => {
                     // Warn if there are unsaved changes
@@ -870,7 +870,7 @@ export default function CanvasPage() {
                   <svg className="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                   </svg>
-                  My Stories
+                  My Canvases
                 </button>
 
                 {/* My Characters */}
